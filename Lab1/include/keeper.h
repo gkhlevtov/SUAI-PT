cfffb915a08117f417ac2e57118ebef69a22fbde
gkhlevtov <gkhlevtov@mail.ru>
2025-10-13 19:33:56 +0300

    Обновлён keeper.h

--- Lab1/include/keeper.h
+++ Lab1/include/keeper.h
@@ -8,13 +8,16 @@ private:
     int size;
 
 public:
-    Keeper();
-    Keeper(const Keeper &);
-    ~Keeper();
+    Keeper();                               // конструктор по умолчанию
+    Keeper(const Keeper &other);            // конструктор копирования
+    Keeper &operator=(const Keeper &other); // присваивание
+    ~Keeper();                              // деструктор
 
     void Add(Conference *obj);
     void Delete(int index);
     void ShowAll() const;
     void SaveToFile(const std::string &path) const;
     void LoadFromFile(const std::string &path);
+    void EditByIndex(int index);
+    int Size() const { return size; }
 };
