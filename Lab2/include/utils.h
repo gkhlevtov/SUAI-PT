5cb73d5fc27417469b9f78fe9061066a301f6905
gkhlevtov <gkhlevtov@mail.ru>
2025-12-06 19:14:16 +0300

    Обновлены utils.h и utils.cpp

--- Lab2/include/utils.h
+++ Lab2/include/utils.h
@@ -3,6 +3,13 @@
 
 #include <string>
 
+// Приведение к нижнему регистру
 std::string toLower(const std::string &s);
+
+// Безопасный ввод числа
+int readInt(const std::string &prompt);
+
+// Безопасный ввод строки
+std::string readLine(const std::string &prompt);
 
 #endif
